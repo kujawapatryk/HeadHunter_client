3a0e2e8c23cc50ec2c8dcb4b260c167bd48127c6
kujawapatryk <homik363@wp.pl>
2023-06-30 16:24:51 +0200

    Improving the routers for the admin section.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
-import { AddHr } from './components/AddHr/AddHr';
-import { Admin } from './components/Admin/Admin';
 import { AuthWrapper } from './components/Auth/AuthWrapper';
 import { Login } from './components/Login/Login';
 import { NewPassword } from './components/NewPassword/NewPassword';
 import { PasswordReminder } from './components/PasswordReminder/PasswordReminder';
-import { SendStudentsData } from './components/SendStudentsData/SendStudentsData';
 import { TestToken } from './components/TestToken/TestToken';
 import { UserChange } from './components/UserChange/UserChange';
-import { CVEdit } from './views/CVEdit/CVEdit';
+import { AdminView } from './views/AdminView/AdminView';
 import { CVView } from './views/CVView/CVView';
 import { ListView } from './views/ListView/ListView';
 import { UserView } from './views/UserView/UserView';
@@ -28,9 +25,7 @@ export const App = () => {
                 <Routes>
                     <Route path="/new-password/:token/:userId" element={<NewPassword />} />
                     <Route path="/password-reset" element={<PasswordReminder />}/>
-                    <Route path="/addhr" element={<AddHr />} />
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="/send-student" element={<SendStudentsData />} />
+                    <Route path="/admin/*" element={<AdminView />} />
                     <Route path="/cv/:studentId" element={<CVView />} />
                     <Route path="/log/:token" element={<TestToken />} />
                     <Route path="/user/*" element={<UserView />} />
@@ -39,9 +34,7 @@ export const App = () => {
                     </Route>
                     <Route path="/" element={<AuthWrapper isLoggedIn={isLoggedIn} />}>
                         <Route path="/list/*" element={<ListView />} />
-                        <Route path="/edit" element={<CVEdit />} />
                     </Route>
-
                 </Routes>
             </SnackbarProvider>
         </>
