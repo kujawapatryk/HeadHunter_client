6ca9ce57101d1f34579a87bc050c41a0a3812ca6
kujawapatryk <homik363@wp.pl>
2023-06-30 16:23:53 +0200

    Adding navigation elements.

--- src/views/UserView/UserView.tsx
+++ src/views/UserView/UserView.tsx
@@ -4,27 +4,32 @@ import { Route, Routes } from 'react-router-dom';
 import { ChangeEmail } from '../../components/ChangeDataUser/ChangeEmail/ChangeEmail';
 import { ChangePassword } from '../../components/ChangeDataUser/ChangePassword/ChangePassword';
 import { FoundJob } from '../../components/ChangeDataUser/FoundJob/FoundJob';
+import { CVEdit } from '../../components/CVEdit/CVEdit';
 import { Header } from '../../components/Header/Header';
 import { NavbarStudents } from '../../components/NavbarStudents/NavbarStudents';
 
+import './UserView.scss';
+import '../../index.scss';
+
 export const UserView = () => {
 
-    const data =[['Edytuj CV','/user/edit'],['Zmień hasło','/user/password'],['Zmień e-mail','/user/email'],['Znalazłem prace','/user/job']];
+    const hr = [['Zmień hasło','/user/password'],['Zmień e-mail','/user/email']];
+    const student =[['Edytuj CV','/user/edit'],['Zmień hasło','/user/password'],['Zmień e-mail','/user/email'],['Znalazłem prace','/user/job']];
 
     return (
         <>
             <Header/>
-            <div className="page-wrapper">
-                <div className="main-wrapper">
-                    <NavbarStudents data={data}/>
-                    <div className="list-wrapper">
+            <div className="page-background__user-view">
+
+                <div className="main-wrapper__user-view">
+                    <NavbarStudents data={student} />
+                    <div className="user-view-wrapper">
                         <Routes>
-                            <Route path="/edit" element={<ChangePassword />}/>
+                            <Route path="/edit" element={<CVEdit />}/>
                             <Route path="/password" element={<ChangePassword />}/>
                             <Route path="/email" element={<ChangeEmail />}/>
                             <Route path="/job" element={<FoundJob />}/>
                         </Routes>
-
                     </div>
                 </div>
             </div>
